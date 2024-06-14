e1537268f5c34734084e36fedbca767c9438f4ee
NERO <mxzxro@gmail.com>
2024-06-14 16:05:04 +0900

    💄 [Resource] enum MeaningColor 추가

---
+++ MeaningOut/Resource.swift
@@ -0,0 +1,17 @@
+//
+//  Resource.swift
+//  MeaningOut
+//
+//  Created by NERO on 6/14/24.
+//
+
+import UIKit
+
+enum MeaningColor {
+    static let primary = UIColor.customOrange
+    static let gray1 = UIColor.customGray1
+    static let gray2 = UIColor.customGray2
+    static let gray3 = UIColor.customGray3
+    static let black = UIColor.black
+    static let white = UIColor.white
+}
