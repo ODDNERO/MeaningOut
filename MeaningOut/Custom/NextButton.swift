33b12e2d67575e04fb4b2c65d086372ba02b1d2c
NERO <mxzxro@gmail.com>
2024-06-14 18:52:17 +0900

    💄 [Custom] NextButton 추가

---
+++ MeaningOut/Custom/NextButton.swift
@@ -0,0 +1,25 @@
+//
+//  NextButton.swift
+//  MeaningOut
+//
+//  Created by NERO on 6/14/24.
+//
+
+import UIKit
+
+class NextButton: UIButton {
+    init(title: String) {
+        super.init(frame: .zero)
+        
+        setTitle(title, for: .normal)
+        setTitleColor(Meaning.Color.white, for: .normal)
+        titleLabel?.font = Meaning.Font.heavy16
+        backgroundColor = Meaning.Color.primary
+        layer.cornerRadius = 23
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
