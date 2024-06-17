038183189bd72cf2f2b0f910efd1c295746e7630
NERO <mxzxro@gmail.com>
2024-06-18 04:56:20 +0900

    ✨ [UIView+] alertDeleteAccount, designateRootVC 메서드 추가

--- MeaningOut/UIViewController+.swift
+++ MeaningOut/UIViewController+.swift
@@ -28,4 +28,24 @@ extension UIViewController {
         ]
         navigationController?.navigationItem.rightBarButtonItem = rightBarItem
     }
+    
+    func alertDeleteAccount(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인", style: .destructive) { _ in
+            self.designateRootVC(OnbordingViewController())
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        present(alert, animated: true)
+    }
+    
+    func designateRootVC(_ VC: UIViewController) {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+        guard let sceneDelegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        let rootViewController = UINavigationController(rootViewController: VC)
+        sceneDelegate.window?.rootViewController = rootViewController
+        sceneDelegate.window?.makeKeyAndVisible()
+    }
 }
