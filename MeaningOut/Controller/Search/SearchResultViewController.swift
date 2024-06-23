12116ed56b269750fc0cf11f0387bdfd881de850
NERO <mxzxro@gmail.com>
2024-06-23 18:23:58 +0900

    ✨ [SearchResultVC] requestProductData() 통신 테스트 완료

--- MeaningOut/Controller/Search/SearchResultViewController.swift
+++ MeaningOut/Controller/Search/SearchResultViewController.swift
@@ -18,7 +18,13 @@ enum Sort: String {
 final class SearchResultViewController: UIViewController {
     var searchKeyword: String = ""
     private let searchResultView = SearchResultView()
-     
+    
+    private var productList: [Item] = [] {
+        willSet {
+            searchResultView.productList = newValue
+        }
+    }
+    
     override func loadView() {
         super.loadView()
         self.view = searchResultView
@@ -31,5 +37,29 @@ final class SearchResultViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         settingNavigation(title: searchKeyword, rightBarItem: nil)
+        requestProductData(sort: .sim)
+    }
+}
+
+extension SearchResultViewController {
+    func requestProductData(sort: Sort) {
+        let url = NaverAPI.baseURL
+        let header: HTTPHeaders = ["X-Naver-Client-Id": NaverAPI.clientID,
+                                   "X-Naver-Client-Secret": NaverAPI.clientSecret]
+        let parameter: Parameters = ["query": searchKeyword,
+                                     "sort": sort.rawValue]
+        
+        AF.request(url, method: .get,
+                   parameters: parameter,
+                   headers: header)
+        .responseDecodable(of: ProductDTO.self) { response in
+            switch response.result {
+            case .success(let product):
+                self.productList = product.items
+                self.searchResultView.productData = product
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
