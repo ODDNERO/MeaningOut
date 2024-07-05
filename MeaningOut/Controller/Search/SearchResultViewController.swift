8be613d0cfc448858d54cfba0ca5a7e79c97c2de
NERO <mxzxro@gmail.com>
2024-07-05 19:16:41 +0900

    ♻️ [SearchResult] CollectionView Delegate/DataSource/DataSourcePrefetching 부분 View -> VC 이동, 불필요한 코드 정리

--- MeaningOut/Controller/Search/SearchResultViewController.swift
+++ MeaningOut/Controller/Search/SearchResultViewController.swift
@@ -8,30 +8,20 @@
 import UIKit
 
 final class SearchResultViewController: UIViewController {
-    var searchKeyword: String = ""
     private let searchResultView = SearchResultView()
-    
     private var productList: [Item] = [] {
-        willSet {
-            searchResultView.productList = newValue
+        didSet {
+            searchResultView.collectionView.reloadData()
         }
     }
     
-    private let itemsPerPage = 30
+    var searchKeyword: String = ""
     private var totalCount = 0
-    private var startData = 1
-    private var isEnd = false {
-        didSet {
-            searchResultView.isEnd = self.isEnd
-        }
-    }
-    
+    private let itemsPerPage = 30
     private lazy var totalPages = (totalCount + itemsPerPage - 1) / itemsPerPage
-    private var page: Int = 1 {
-        didSet {
-            searchResultView.page = self.page
-        }
-    }
+    private var page = 1
+    private var startData = 1
+    private var isEnd = false
     
     override func loadView() {
         super.loadView()
@@ -40,6 +30,7 @@ final class SearchResultViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        settingDelegate()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -53,14 +44,18 @@ final class SearchResultViewController: UIViewController {
             }
         }
         incrementPage()
-        
-        searchResultView.navigationController = self.navigationController
     }
 }
 
 extension SearchResultViewController {
+    private func settingDelegate() {
+        searchResultView.collectionView.delegate = self
+        searchResultView.collectionView.dataSource = self
+        searchResultView.collectionView.register(SearchResultCollectionViewCell.self, forCellWithReuseIdentifier: SearchResultCollectionViewCell.identifier)
+        searchResultView.collectionView.prefetchDataSource = self
+    }
+    
     private func incrementPage() {
-        searchResultView.nextPage = {
             self.page += 1
             self.startData += self.itemsPerPage
             SearchManager.shared.requestProductData(self.searchKeyword, sort: .sim, itemsPerPage: self.itemsPerPage, start: self.startData) { product, error in
@@ -71,7 +66,6 @@ extension SearchResultViewController {
                     self.paginateData(data: product)
                 }
             }
-        }
     }
     
     private func paginateData(data: Product) {
@@ -83,5 +77,36 @@ extension SearchResultViewController {
         }
         self.searchResultView.update(data: data)
         self.isEnd = self.page == self.totalPages
+    }
+}
+
+extension SearchResultViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return productList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchResultCollectionViewCell.identifier, for: indexPath) as! SearchResultCollectionViewCell
+        cell.update(data: productList[indexPath.row])
+        
+        cell.productImageView.tag = indexPath.row
+        cell.productImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(productImageClicked)))
+        return cell
+    }
+    @objc func productImageClicked(sender: UITapGestureRecognizer) {
+    let productWebVC = ProductWebViewController()
+        productWebVC.selectedProductName = productList[sender.view!.tag].title
+        productWebVC.productURL = productList[sender.view!.tag].link
+        navigationController?.pushViewController(productWebVC, animated: true)
+    }
+}
+
+extension SearchResultViewController: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        for item in indexPaths {
+            if productList.count - 2 == item.row && isEnd == false {
+                incrementPage()
+            }
+        }
     }
 }
--- MeaningOut/View/Search/SearchResultView.swift
+++ MeaningOut/View/Search/SearchResultView.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 class SearchResultView: UIView {
-    var navigationController: UINavigationController? = nil
-    
     private var searchResultLabel = {
         let label = UILabel()
         label.textColor = Meaning.Color.primary
@@ -71,14 +69,6 @@ class SearchResultView: UIView {
         layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 10, right: 20)
         return layout
     }
-    var productList: [Item] = [] {
-        didSet {
-            collectionView.reloadData()
-        }
-    }
-    var page: Int = 1
-    var isEnd = false
-    var nextPage: (() -> Void)?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -92,46 +82,10 @@ class SearchResultView: UIView {
     }
 }
 
-extension SearchResultView: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return productList.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchResultCollectionViewCell.identifier, for: indexPath) as! SearchResultCollectionViewCell
-        cell.update(data: productList[indexPath.row])
-        
-        cell.productImageView.tag = indexPath.row
-        cell.productImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(productImageClicked)))
-        return cell
-    }
-    
-    @objc func productImageClicked(sender: UITapGestureRecognizer) {
-    let productWebVC = ProductWebViewController()
-        productWebVC.selectedProductName = productList[sender.view!.tag].title
-        productWebVC.productURL = productList[sender.view!.tag].link
-        navigationController?.pushViewController(productWebVC, animated: true)
-    }
-}
-
-extension SearchResultView: UICollectionViewDataSourcePrefetching {
-    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
-        for item in indexPaths {
-            if productList.count - 2 == item.row && isEnd == false {
-                nextPage?()
-            }
-        }
-    }
-}
-
 extension SearchResultView {
     private func configureSetting() {
         self.backgroundColor = Meaning.Color.background
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(SearchResultCollectionViewCell.self, forCellWithReuseIdentifier: SearchResultCollectionViewCell.identifier)
         setClickedButtonUI(accuracySortButton)
-        collectionView.prefetchDataSource = self
     }
     
     func update(data: Product) {
@@ -146,7 +100,6 @@ extension SearchResultView {
             $0.layer.borderWidth = 1
             $0.layer.borderColor = Meaning.Color.gray1.cgColor
         }
-        
         sender.backgroundColor = Meaning.Color.gray3
         sender.setTitleColor(.white, for: .normal)
         sender.layer.borderWidth = 0
@@ -164,13 +117,11 @@ extension SearchResultView {
             $0.top.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(20)
             $0.leading.equalTo(self.safeAreaLayoutGuide).inset(20)
         }
-        
         filterStackView.snp.makeConstraints {
             $0.top.equalTo(searchResultLabel.snp.bottom).offset(12)
             $0.leading.equalTo(self.safeAreaLayoutGuide).inset(20)
             $0.height.equalTo(50)
         }
-
         collectionView.snp.makeConstraints {
             $0.top.equalTo(filterStackView.snp.bottom)
             $0.horizontalEdges.bottom.equalTo(self.safeAreaLayoutGuide)
