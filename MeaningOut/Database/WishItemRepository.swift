62862ba620de65c8421f09c7ea0e66874bf72d01
NERO <mxzxro@gmail.com>
2024-07-06 01:39:26 +0900

    ✨ WishItemRepository 추가

---
+++ MeaningOut/Database/WishItemRepository.swift
@@ -0,0 +1,61 @@
+//
+//  WishItemRepository.swift
+//  MeaningOut
+//
+//  Created by NERO on 7/5/24.
+//
+
+import Foundation
+import RealmSwift
+
+final class WishItemRepository {
+    private let realm = try! Realm()
+    
+    func createItem(_ data: WishItem) {
+        do {
+            try realm.write {
+                realm.add(data)
+            }
+        } catch {
+            print("Create Error: \(error)")
+        }
+    }
+    
+    func readAllItem() -> Results<WishItem> {
+        return realm.objects(WishItem.self).sorted(byKeyPath: "wishDate", ascending: true)
+    }
+    
+    func updateItem<T>(_ data: WishItem, coulmn: String, value: T) {
+        realm.create(WishItem.self, value: ["id": data.id, coulmn: value], update: .modified)
+    }
+    
+    func updateAllItem<T>(coulmn: String, value: T) {
+        let result = realm.objects(WishItem.self)
+        try! realm.write {
+            result.setValue(value, forKey: coulmn)
+        }
+    }
+    
+    func deleteItem(_ data: WishItem) {
+        do {
+            try realm.write {
+                realm.delete(data)
+            }
+        } catch {
+            print("Delete Error: \(error)")
+        }
+    }
+    
+    func filterSearchTitleItem(column: String, searchText: String) -> Results<WishItem> {
+        let filter = realm.objects(WishItem.self).where {
+            $0.title.contains(searchText)
+        }
+        let result = searchText.isEmpty ? realm.objects(WishItem.self) : filter
+        return result
+    }
+    
+    func isItemInWishlist(productID: String) -> Bool {
+        let item = realm.objects(WishItem.self).filter("productID == %@", productID).first
+        return item != nil
+    }
+}
