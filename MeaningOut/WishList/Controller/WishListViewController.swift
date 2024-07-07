63f9cbe6f3e6518ac273be99215a55acd3f3f262
NERO <mxzxro@gmail.com>
2024-07-07 23:31:45 +0900

    ✨ [WishListVC] Wish Button 클릭 시 Menu 버튼 띄워 아이템 삭제 기능 구현
    //주석 Menu 대신 Alert 구현 코드

--- MeaningOut/WishList/Controller/WishListViewController.swift
+++ MeaningOut/WishList/Controller/WishListViewController.swift
@@ -52,6 +52,27 @@ extension WishListViewController: UITableViewDelegate, UITableViewDataSource {
         if let item = wishList?[indexPath.row] { cell.update(data: item) }
         return cell
     }
+    @objc func wishButtonClicked(_ sender: UIButton) {
+        let indexPath = IndexPath(item: sender.tag, section: 0)
+        if let cell = wishListView.wishTableView.cellForRow(at: indexPath) as? WishListTableViewCell {
+            guard let selectedItemProductID = self.wishList?[indexPath.row].productID else { return }
+            
+            let deleteAction = UIAction(title: "위시에서 빼기", attributes: .destructive, handler: { _ in
+                self.repository.deleteItem(selectedItemProductID)
+                self.wishListView.wishTableView.reloadData()
+                print(self.repository.readAllItem())
+            })
+            cell.wishButton.menu = UIMenu(children: [deleteAction])
+            cell.wishButton.showsMenuAsPrimaryAction = true
+        }
+//        showAlert(title: "위시에서 삭제할까요?", message: "", okText: "삭제") {
+//            if let cell = self.wishListView.wishTableView.cellForRow(at: indexPath) as? WishListTableViewCell {
+//                cell.configureWishButtonUI(false)
+//                guard let selectedItemProductID = self.wishList?[indexPath.row].productID else { return }
+//                self.repository.deleteItem(selectedItemProductID)
+//            }
+//        }
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 120
