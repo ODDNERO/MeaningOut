748ac18664da497194934f33130bafc75b265296
NERO <mxzxro@gmail.com>
2024-07-07 23:23:24 +0900

    ✨ [WishListVC] Realm WishItem 목록 TableView 띄우기 완료

--- MeaningOut/WishList/Controller/WishListViewController.swift
+++ MeaningOut/WishList/Controller/WishListViewController.swift
@@ -6,9 +6,17 @@
 //
 
 import UIKit
+import RealmSwift
 
 final class WishListViewController: UIViewController {
     private let wishListView = WishListView()
+    
+    private let repository = WishItemRepository()
+    private var wishList: Results<WishItem>? {
+        didSet {
+            wishListView.wishTableView.reloadData()
+        }
+    }
     
     override func loadView() {
         self.view = wishListView
@@ -16,6 +24,30 @@ final class WishListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        settingDelegate()
+        wishList = repository.readAllItem()
+        wishListView.wishTableView.reloadData()
+    }
+}
+extension WishListViewController {
+    private func settingDelegate() {
+        wishListView.wishTableView.delegate = self
+        wishListView.wishTableView.dataSource = self
+        wishListView.wishTableView.register(WishListTableViewCell.self, forCellReuseIdentifier: WishListTableViewCell.identifier)
+}
+
+extension WishListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return wishList?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: WishListTableViewCell.identifier) as! WishListTableViewCell
+        if let item = wishList?[indexPath.row] { cell.update(data: item) }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
     }
 }
